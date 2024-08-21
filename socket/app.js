a1ff675c1bd448f57a74007a116b5363ad535813
Manshi <bhattmanshi66@gmail.com>
2024-08-21 14:50:16 +0530

    add

--- socket/app.js
+++ socket/app.js
@@ -2,9 +2,9 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://real-estate-kwif.vercel.app",
   },
-});
+}); 
 
 let onlineUser = [];
 
@@ -13,15 +13,13 @@ const addUser = (userId, socketId) => {
   if (!userExits) {
     onlineUser.push({ userId, socketId });
   }
-};
+}; 
 
 const removeUser = (socketId) => {
   onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
 };
 
-// const getUser = (userId) => {
-//   return onlineUser.find((user) => user.userId === userId);
-// };
+
 
 
 const getUser = (userId) => {
@@ -31,13 +29,10 @@ const getUser = (userId) => {
 io.on("connection", (socket) => {
   socket.on("newUser", (userId) => {
     addUser(userId, socket.id);
-    // console.log('Current online users:', onlineUser);
   });
 
   socket.on("sendMessage", ({ receiverId, data }) => {
-    // console.log(receiverId)
     const receiver = getUser(receiverId); 
-   // console.log(receiver) 
     io.to(receiver.socketId).emit("getMessage", data);
   });
 
