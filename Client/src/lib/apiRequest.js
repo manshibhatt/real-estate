9b21b691fc80962b1746573efb7f5cb8358227d2
Manshi <bhattmanshi66@gmail.com>
2024-08-21 10:26:20 +0530

    add /api

--- Client/src/lib/apiRequest.js
+++ Client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-backend-s12g.onrender.com",
+  baseURL: "https://real-estate-backend-s12g.onrender.com/api",
   withCredentials: true,
 });
 
